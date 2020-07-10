e65a4ffc82a9fc38f8881ffe3ca07c64f762abce
Tristan Elliott <tristanelliott7@gmail.com>
2020-07-10 17:31:50 -0300

    the basic navBar

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React,{ useState } from 'react';
+import {useSpring,animated,config} from 'react-spring';
 
 
 
 
 const NavBar = ()=>{
+  const [show,setshow] = useState(false)
+  const props = useSpring({opacity:show? 1:0,config:config.wobbly})
+
+  const handleClick =()=>{
+    setshow(!show)
+  }
 
   return(
-    <nav>NavBar</nav>
+
+        <nav>
+          <div className="logo">Logo</div>
+          <div onClick={()=>{handleClick()}}><i className="fas fa-bars"></i></div>
+          <ul className={show? '': 'navbar-close'} >
+            <animated.li style={props}>Contact</animated.li>
+            <animated.li style={props}>About us</animated.li>
+            <animated.li style={props}>Blog</animated.li>
+            <animated.li style={props}>Ads</animated.li>
+            <animated.li style={props}>Gallery</animated.li>
+          </ul>
+        </nav>
+
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 
+import './Accessories/Sass/base.scss'
 import App from './Components/App';
+
 
 
 ReactDOM.render(<App />,document.getElementById('root'))
